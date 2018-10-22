e4d7ece335d9d61b2d452c40bb49082c0b75bd6b
Ezequiel <ezebongiovi@gmail.com>
2018-10-22 19:56:09 -0300

    Networking setup

---
+++ app/src/main/java/com/weekendwars/movieschallenge/networking/RetrofitAdapter.java
@@ -0,0 +1,43 @@
+package com.weekendwars.movieschallenge.networking;
+
+import android.support.annotation.NonNull;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.weekendwars.movieschallenge.BuildConfig;
+import com.weekendwars.movieschallenge.networking.interceptors.APIKeyInterceptor;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public final class RetrofitAdapter {
+
+    public static final RetrofitAdapter INSTANCE = new RetrofitAdapter();
+    private final Retrofit mRetrofit;
+
+    private RetrofitAdapter() {
+        final Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create();
+
+        mRetrofit = new Retrofit.Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .client(getClient())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+    }
+
+
+    @NonNull
+    public Retrofit getRetrofit() {
+        return mRetrofit;
+    }
+
+    private OkHttpClient getClient() {
+        return new OkHttpClient.Builder().addInterceptor(new APIKeyInterceptor()).build();
+    }
+}
---
+++ app/src/main/java/com/weekendwars/movieschallenge/networking/interceptors/APIKeyInterceptor.java
@@ -0,0 +1,31 @@
+package com.weekendwars.movieschallenge.networking.interceptors;
+
+import android.support.annotation.NonNull;
+
+import com.weekendwars.movieschallenge.BuildConfig;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Authentication interceptor intended to add the API KEY as a query param on every request
+ */
+public class APIKeyInterceptor implements Interceptor {
+
+    private static final String QUERY_PARAM_API_KEY = "api_key";
+
+    @Override
+    public Response intercept(@NonNull final Chain chain) throws IOException {
+        Request request = chain.request();
+        final HttpUrl url = request.url().newBuilder()
+                .addQueryParameter(QUERY_PARAM_API_KEY, BuildConfig.API_KEY).build();
+
+        request = request.newBuilder().url(url).build();
+
+        return chain.proceed(request);
+    }
+}
